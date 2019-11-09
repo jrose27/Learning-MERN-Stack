b70cc9ac5dd78b332f0ab7fd44902de5d8be125b
John Rose <jh.rose.37@gmail.com>
2019-11-09 10:25:25 -0500

    Finished adding Redux and added deleteItem and addItem functionality

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,6 +1,7 @@
 import uuid from 'uuid';
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
 
+// items will come from backend
 const intialState = {
   items: [
     { id: uuid(), name: 'Eggs' },
@@ -12,11 +13,26 @@ const intialState = {
 
 export default function(state = intialState, action) {
   switch(action.type) {
+
     case GET_ITEMS:
       return {
         ...state
-      }
+      };
+
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter(item => item.id !== action.payload)
+      };
+
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload, ...state.items]
+      };
+
     default:
       return state;
-  }
-}
+
+  } // Closes switch
+} // Closes export function
