7e5f398f84fa789cca22e8cdcb4b286ba1d0192a
John Rose <jh.rose.37@gmail.com>
2019-11-23 01:07:42 -0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 // mongodb+srv://<username>:<password>@cluster0-8z5s4.mongodb.net/admin?retryWrites=true&w=majority
 module.exports = {
-  mongoURI:'mongodb+srv://default:default123@cluster0-8z5s4.mongodb.net/test?retryWrites=true&w=majority'
+  mongoURI:'Get_It_Girl'
 };
